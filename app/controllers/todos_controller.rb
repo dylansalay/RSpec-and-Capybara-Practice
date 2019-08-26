31dc44ef0a310e01beb24fbe92cbdc48c5ed3065
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2019-08-25 20:05:01 -0700

    Finished Video 4

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,21 @@
+class TodosController < ApplicationController
+  before_action :authenticate
+
+  def index
+    @todos = Todo.where(email: session[:current_email])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    Todo.create(todo_params.merge(email: session[:current_email]))
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title )
+  end
+end
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "User creates todo" do
+  scenario "Successfully" do
+    sign_in
+    click_on "Add a new todo"
+    fill_in "Title", with: "Buy milk"
+    click_on "Submit"
+    expect(page).to have_css '.todos li', text: "Buy milk"
+  end
+end
